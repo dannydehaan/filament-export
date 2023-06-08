f1939ffc6985bf5cda3c7163fc6113657327e335
Danny the Rooster <danny@pixels.nl>
2023-06-08 06:02:58 +0200

    Lang NL

---
+++ resources/lang/nl/export_action.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'action_label' => 'Exporteren',
+    'modal_heading' => 'Exporteren',
+    'file_name_field_label' => 'Bestandsnaam',
+    'format_field_label' => 'Extensie',
+    'page_orientation_field_label' => 'Paginaoriëntatie',
+    'filter_columns_field_label' => 'Filter kolommen',
+    'additional_columns_field' => [
+        'label' => 'Extra kolommen',
+        'title_field_label' => 'Titel',
+        'default_value_field_label' => 'Standaard waarde',
+        'add_button_label' => 'Kolom toevoegen',
+    ],
+    'export_action_label' => 'Exporteren',
+    'print_action_label' => 'Printen',
+    'preview_action_label' => 'Voorbeeld',
+    'cancel_action_label' => 'Annuleren',
+    'page_orientation_portrait' => 'Portret',
+    'page_orientation_landscape' => 'Landschap',
+];
---
+++ resources/lang/nl/table_view.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'preview_modal_heading' => 'Voorbeeld',
+    'print_action_label' => 'Printen',
+    'export_action_label' => 'Exporteren',
+];
